f85ed9423b56132e147d9f6e9ba757bc4618a324
montanpi <12225722+montanpi@users.noreply.github.com>
2019-07-18 04:07:56 +0900

    Style email list

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -32,18 +32,19 @@
             v-bind:checked="ids.indexOf(email.id) !== -1"
             @click="toggleSelected(email.id)"
           />
-          <span class="grid-icon"></span>
-          <span class="grid-from">{{ email.from }}</span>
-          <span class="grid-to">{{ formatRecipients(email.to) }}</span>
-          <span
+          <div class="grid-icon"></div>
+          <div class="grid-from">{{ email.from }}</div>
+          <div class="grid-to">{{ formatRecipients(email.to) }}</div>
+          <div
             v-if="email.to.length > 1"
             class="grid-badge"
             style="color: red"
-          >{{ formatBadge(email.to) }}</span>
-          <span class="grid-subject">{{ email.subject }}</span>
+          >{{ formatBadge(email.to) }}</div>
+          <div class="grid-subject">{{ email.subject }}</div>
           <div class="grid-date">
-            <img v-if="email.attachment" src="../assets/icon_clip.svg" alt="icon_clip">
+            <img v-if="email.attachment" class="date-attachment" src="../assets/icon_clip.svg" alt="icon_clip" />
             <span>{{ formatDateWrapper(email.date) }}</span>
+            <img class="date-arrow" src="../assets/icon_arrow02.svg" alt="icon_arrow02" />
           </div>
         </div>
       </div>
@@ -167,68 +168,69 @@ export default {
     }
   }
 }
-  .email-grid {
-    display: grid;
-    box-sizing: border-box;
-    width: 520px;
-    height: 126px;
-    border-bottom: em(1) solid $border-color;
-    grid-template-columns: em(39) em(324) em(22) em(102);
-    grid-template-rows: em(30) em(30) em(38);
-    padding: em(14) em(16) em(12) em(16);
+.email-grid {
+  display: grid;
+  box-sizing: border-box;
+  width: em(520);
+  height: em(126);
+  border-bottom: em(1) solid $border-color;
+  grid-template-columns: em(39) em(324) em(22) em(102);
+  grid-template-rows: em(30) em(30) em(38);
+  padding: em(14) em(16) em(12) em(16);
+  &:last-child {
+    border-bottom: 0;
   }
-  .grid-checkbox {
-    display: none;
+}
+.grid-checkbox {
+  display: none;
+}
+.grid-icon {
+  grid-area: 1 / 1 / 3 / 2;
+  background: url("../assets/icon_mail_sp.svg") left em(13) center / em(16)
+    no-repeat;
+}
+.grid-from {
+  grid-area: 1 / 2 / 1 / 3;
+  padding: em(2) 0;
+  font-size: em(18.9);
+  font-weight: bold;
+  color: $selected-font-color;
+}
+.grid-to {
+  grid-area: 2 / 2 / 2 / 4;
+  padding: em(2) 0;
+  font-size: em(18.9);
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.grid-badge {
+  grid-area: 2 / 4 / 2 / 5;
+}
+.grid-subject {
+  grid-area: 3 / 1 / 3 / 5;
+  padding-top: em(3);
+  font-size: em(22);
+  padding-left: em(8);
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.grid-date {
+  grid-area: 1 / 3 / 1 / 5;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  font-size: em(18.5);
+  .date-arrow {
+    width: auto;
+    height: em(8);
+    padding: em(5);
   }
-  .grid-icon {
-    grid-area: 1 / 1 / 3 / 2;
-    background: url("../assets/icon_mail_sp.svg") left em(13) center / em(16) no-repeat;
+  .date-attachment {
+    width: auto;
+    height: em(18);
+    padding: em(8);
   }
-  .grid-from {
-    grid-area: 1 / 2 / 1 / 3;
-    padding: em(2) 0;
-    font-size: em(18.9);
-    font-weight: bold;
-    color: $selected-font-color;
-  }
-  .grid-to {
-    grid-area: 2 / 2 / 2 / 4;
-    padding: em(2) 0;
-    font-size: em(18.9);
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
-  .grid-badge {
-    grid-area: 2 / 4 / 2 / 5;
-  }
-  .grid-subject {
-    grid-area: 3 / 1 / 3 / 5;
-    padding: em(3) 0;
-    font-size: em(22);
-    padding-left: em(8);
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
-  .grid-date {
-    grid-area: 1 / 3 / 1 / 5;
-    span {
-      padding: em(4) 0;
-      font-size: em(17.4);
-    }
-    img {
-      width: em(17.5);
-      height: auto;
-    }
-    &::after {
-      background: url("../assets/icon_arrow02.svg") right no-repeat;
-      background-size: contain;
-      content: "";
-      display: inline-block;
-      vertical-align: middle;
-      width: em(12);
-      height: em(8);
-    }
-  }
+}
 </style>
